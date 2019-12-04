9800edd0aef77b5bee20060dd19341fe323beb78
Abel C Dixon <abelcheruvathoor@gmail.com>
2019-12-05 02:47:31 +0530

    Create Reciever_Arduino_UNO.ino

---
+++ Reciever_Arduino_UNO.ino
@@ -0,0 +1,139 @@
+/*
+    DIY Arduino based RC Transmitter Project
+              == Receiver Code ==
+  by Dejan Nedelkovski, www.HowToMechatronics.com
+  Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define channel_number 6  //set the number of channels
+#define sigPin 2  //set PPM signal output pin on the arduino
+#define PPM_FrLen 27000  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PPM_PulseLen 400  //set the pulse length
+
+int ppm[channel_number];
+
+
+RF24 radio(7, 8);   // nRF24L01 (CE, CSN)
+const byte address[6] = "00001";
+unsigned long lastReceiveTime = 0;
+unsigned long currentTime = 0;
+// Max size of this struct is 32 bytes - NRF24L01 buffer limit
+struct Data_Package {
+  byte yaw;
+  byte roll;
+  byte pitch;
+  byte throttle;
+  byte tSwitch1;
+  byte tSwitch2;
+};
+Data_Package data; //Create a variable with the above structure
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.startListening(); //  Set the module as receiver
+  resetData();
+  setupPPM();
+}
+void loop() {
+  // Check whether there is data to be received
+  if (radio.available()) {
+    radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
+    lastReceiveTime = millis(); // At this moment we have received the data
+  }
+  // Check whether we keep receving data, or we have a connection between the two modules
+  currentTime = millis();
+  if ( currentTime - lastReceiveTime > 1000 ) { // If current time is more then 1 second since we have recived the last data, that means we have lost connection
+    resetData(); // If connection is lost, reset the data. It prevents unwanted behavior, for example if a drone has a throttle up and we lose connection, it can keep flying unless we reset the values
+  }
+  // Print the data in the Serial Monitor
+  Serial.print("yaw: ");
+  Serial.print(data.yaw);
+  Serial.print("; roll: ");
+  Serial.print(data.roll);
+  Serial.print("; pitch: ");
+  Serial.print(data.pitch);
+  Serial.print("; throttle: ");
+  Serial.print(data.throttle); 
+  Serial.print("; tSwitch1: ");
+  Serial.print(data.tSwitch1); 
+  Serial.print("; tSwitch2: ");
+  Serial.println(data.tSwitch2); 
+  setPPMValuesFromData();
+}
+void resetData() {
+  data.roll = 127;
+  data.yaw = 127;
+  data.pitch = 127;
+  data.throttle = 0;
+  data.tSwitch1 = 0;
+  data.tSwitch2 = 0;
+  setPPMValuesFromData();
+}
+
+void setPPMValuesFromData()
+{
+  ppm[0] = map(data.throttle, 0, 255, 1000, 2000);
+  ppm[1] = map(data.yaw,      0, 255, 1000, 2000);
+  ppm[2] = map(data.pitch,    0, 255, 1000, 2000);
+  ppm[3] = map(data.roll,     0, 255, 1000, 2000);
+  ppm[4] = map(data.tSwitch1,     0, 1, 1000, 2000);
+  ppm[5] = map(data.tSwitch2,     0, 1, 1000, 2000);  
+  }
+
+void setupPPM() {
+  pinMode(sigPin, OUTPUT);
+  digitalWrite(sigPin, 0);  //set the PPM signal pin to the default state (off)
+
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+
+  OCR1A = 100;  // compare match register (not very important, sets the timeout for the first interrupt)
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+}
+
+//#error Delete this line befor you cahnge the value (clockMultiplier) below
+#define clockMultiplier 2 // set this to 2 if you are using a 16MHz arduino, leave as 1 for an 8MHz arduino
+
+ISR(TIMER1_COMPA_vect){
+  static boolean state = true;
+
+  TCNT1 = 0;
+
+  if ( state ) {
+    //end pulse
+    PORTD = PORTD & ~B00000100; // turn pin 2 off. Could also use: digitalWrite(sigPin,0)
+    OCR1A = PPM_PulseLen * clockMultiplier;
+    state = false;
+  }
+  else {
+    //start pulse
+    static byte cur_chan_numb;
+    static unsigned int calc_rest;
+
+    PORTD = PORTD | B00000100; // turn pin 2 on. Could also use: digitalWrite(sigPin,1)
+    state = true;
+
+    if(cur_chan_numb >= channel_number) {
+      cur_chan_numb = 0;
+      calc_rest += PPM_PulseLen;
+      OCR1A = (PPM_FrLen - calc_rest) * clockMultiplier;
+      calc_rest = 0;
+    }
+    else {
+      OCR1A = (ppm[cur_chan_numb] - PPM_PulseLen) * clockMultiplier;
+      calc_rest += ppm[cur_chan_numb];
+      cur_chan_numb++;
+    }     
+  }
+}
